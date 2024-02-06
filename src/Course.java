620ae62fffd23d9fb9f6468c87777e9455be1c6e
Jacob <jacobmac9991@gmail.com>
2024-02-05 21:37:35 -0400

    Created constructors for classes

--- src/Course.java
+++ src/Course.java
@@ -1,2 +1,8 @@
 public class Course {
+    public String course_name;
+
+    public Course(String course_name) {
+        this.course_name = course_name;
+    }
+
 }
--- src/Student.java
+++ src/Student.java
@@ -3,4 +3,9 @@ public class Student {
     public int a1_mark;
     public int a2_mark;
 
+    public Student(String student_name, int a1_mark, int a2_mark) {
+        this.student_name = student_name;
+        this.a1_mark = a1_mark;
+        this.a2_mark = a2_mark;
+    }
 }
